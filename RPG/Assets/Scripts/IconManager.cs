35e32e7f701ef4aa5ca43c421cd83b1d5357ed99
KID421 <suchild7@gmail.com>
2019-07-21 17:19:23 +0800

    轉移專案

---
+++ RPG/Assets/Scripts/IconManager.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;  // 引用 場景管理 API
+
+public class IconManager : MonoBehaviour
+{
+    [Header("圖示")]
+    public RectTransform Icon;
+    [Header("編號")]
+    public int Index;
+    [Header("座標")]
+    public Vector2[] Position = { new Vector2(365, -50), new Vector2(365, -130), new Vector2(465, -200) };
+    [Header("音效")]
+    public AudioSource AS;
+    public AudioClip SoundSelect, SoundEnter;
+
+    private void ControlIcon(int number)
+    {
+        AS.PlayOneShot(SoundSelect, 1.5f);
+
+        Index += number;                            // 編號累加傳入的數字
+
+        if (Index == 3) Index = 0;
+        else if (Index == -1) Index = 2;
+
+        Icon.anchoredPosition = Position[Index];    // 圖示 的 座標 = 陣列資料[編號]
+    }
+
+    private void Enter()
+    {
+        AS.PlayOneShot(SoundEnter, 1.5f);
+
+        if (Index == 0)
+        {
+            //GameStart();                  // 直接執行方法
+            Invoke("GameStart", 1.5f);      // 延遲執行方法 ("方法名稱", 延遲時間)
+        }
+        else if (Index == 1)
+        {
+            //GameQuit();
+            Invoke("GameQuit", 1.5f);
+        }
+        else if (Index == 2)
+        {
+            //GameSetting();
+            Invoke("GameSetting", 1.5f);
+        }
+    }
+
+    private void GameStart()
+    {
+        SceneManager.LoadScene("遊戲場景");
+    }
+
+    private void GameQuit()
+    {
+        Application.Quit();
+    }
+
+    private void GameSetting()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))         // 按下 S 或者 下 控制圖示 - 傳入 +1 往下跑
+        {
+            ControlIcon(1);
+        }
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))           // 按下 W 或者 上 控制圖示 - 傳入 -1 往上跑
+        {
+            ControlIcon(-1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))   // Enter 鍵 Return
+        {
+            Enter();
+        }
+    }
+}
---
+++ RPG/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,147 @@
+﻿using UnityEngine;
+using UnityEngine.UI;   // 引用介面 API
+using Fungus;           // 引用蘑菇 API
+
+public enum NPCState
+{
+    鐵塊, 鐵塊不足, 給予鑰匙, 打敗魔王
+}
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("走路速度")]
+    public float speed;
+    [Header("旋轉速度")]
+    public float turn;
+    [Header("剛體")]
+    public Rigidbody rig;
+    [Header("動畫控制器")]
+    public Animator ani;
+    [Header("村民流程圖")]
+    public Flowchart npc;
+    [Header("村民目前狀態")]
+    public NPCState npcState;
+    [Header("任務介面")]
+    public Text textMission;
+    [Header("鐵礦數量")]
+    public int metalCount;
+
+    private GameObject obj;     // 儲存觸碰到的物件
+
+    private void Move()
+    {
+        // 如果 目前動畫的資訊 名稱為"攻擊" 就跳出
+        if (ani.GetCurrentAnimatorStateInfo(0).IsName("攻擊"))
+        {
+            return;
+        }
+
+        // Vertical   垂直：上：1、下：-1、沒按：0
+        // Horizontal 水平：右：1、左：-1、沒按：0
+        float v = Input.GetAxisRaw("Vertical");
+        float h = Input.GetAxisRaw("Horizontal");
+        // Mouse X 滑鼠水平：左：負，右：正、沒按：0
+        float mX = Input.GetAxisRaw("Mouse X");
+
+        //Debug.Log("玩家垂直：" + v);
+        //Debug.Log("玩家水平：" + h);
+        //Debug.Log("滑鼠水平：" + mX);
+
+        // 剛體 推力((角色前方 * 垂直 + 角色右方 * 水平) * 速度)
+        rig.AddForce((transform.forward * v + transform.right * h) * speed);
+        // 變形 旋轉(X，Y，Z)
+        transform.Rotate(0, mX * turn, 0);
+    }
+
+    private void AnimationControl()
+    {
+        if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1 || Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1)
+        {
+            ani.SetBool("跑步開關", true);
+        }
+        else if(Input.GetAxisRaw("Vertical") == 0 || Input.GetAxisRaw("Horizontal") == 0)
+        {
+            ani.SetBool("跑步開關", false);
+        }
+
+        // 如果 目前動畫的資訊 名稱為"攻擊" 就重新設定 "攻擊開關"
+        if (ani.GetCurrentAnimatorStateInfo(0).IsName("攻擊"))
+        {
+            ani.ResetTrigger("攻擊開關");
+        }
+
+        // 按下左鍵 播放攻擊動畫
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            ani.SetTrigger("攻擊開關");
+        }
+    }
+
+    private void Update()
+    {
+        Move();
+        AnimationControl();
+    }
+
+    private void Start()
+    {
+        Cursor.visible = false; // 指標 顯示 = 不要顯示 (true 為顯示)
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        //Debug.Log("觸發到的物件：" + other.name);
+
+        if (other.name == "村民")
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                //Debug.Log("觸發對話!!!");
+
+                switch (npcState)
+                {
+                    case NPCState.鐵塊:
+                        npc.SendFungusMessage("鐵塊");
+                        //npcState = NPCState.鐵塊不足;
+                        break;
+                    case NPCState.鐵塊不足:
+                        npc.SendFungusMessage("鐵塊不足");
+                        break;
+                    case NPCState.給予鑰匙:
+                        npc.SendFungusMessage("給予鑰匙");
+                        break;
+                    case NPCState.打敗魔王:
+                        npc.SendFungusMessage("打敗魔王");
+                        break;
+                }
+            }
+        }
+
+        if (other.tag == "鐵礦")
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                //Destroy(other.gameObject);
+                obj = other.gameObject;                     // 物件 = 觸碰.遊戲物件
+                Invoke("DelayDestroyMetal", 1);             // 延遲一秒呼叫方法：DelayDestroyMetal
+            }
+        }
+    }
+
+    private void DelayDestroyMetal()
+    {
+        if (obj == null) return;
+        metalCount++;                                               // 數量遞增
+        textMission.text = "取得鐵礦：" + metalCount + " / 5";      // 更新介面
+        Destroy(obj);                                               // 刪除(物件)
+    }
+
+    /// <summary>
+    /// 改變 NPC 村民狀態，0 鐵塊，1 鐵塊不足，2 給予鑰匙，3 打敗魔王
+    /// </summary>
+    /// <param name="npcStateIndex">編號</param>
+    public void ChangeNPCState(int npcStateIndex)
+    {
+        npcState = (NPCState)npcStateIndex;
+    }
+}
